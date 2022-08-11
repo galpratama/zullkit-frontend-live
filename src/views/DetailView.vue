e32e611cdbd6ff2deb1ff1f81e62392361e749d2
Galih Pratama <hanamura.iost@gmail.com>
2022-08-11 17:59:50 +0700

    Update

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -135,30 +135,30 @@ onMounted(() => {
                 >
                   Download Now
                 </a>
-                <RouterLink
-                  v-else
-                  to="/pricing"
-                  class="
-                    inline-flex
-                    items-center
-                    justify-center
-                    w-full
-                    px-8
-                    py-3
-                    text-base
-                    font-medium
-                    text-white
-                    bg-indigo-600
-                    border border-transparent
-                    rounded-full
-                    hover:bg-indigo-700
-                    md:py-2 md:text-md md:px-10
-                    hover:shadow
-                  "
-                >
-                  Subscribe
-                </RouterLink>
               </div>
+              <RouterLink
+                v-else
+                to="/pricing"
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  w-full
+                  px-8
+                  py-3
+                  text-base
+                  font-medium
+                  text-white
+                  bg-indigo-600
+                  border border-transparent
+                  rounded-full
+                  hover:bg-indigo-700
+                  md:py-2 md:text-md md:px-10
+                  hover:shadow
+                "
+              >
+                Subscribe
+              </RouterLink>
             </div>
           </div>
         </aside>
